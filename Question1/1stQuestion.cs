551f4852e95a647e1b80f350209f49841a1f36be
Innhahn <jaytan3825@gmail.com>
2023-04-25 17:45:52 +0800

    First Commit, Included the two questions seperated by a different folder

---
+++ Question1/1stQuestion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AnalogueClockAngle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give the input hours and minutes (you have only 5 tries)");
+            int hours = 0;
+            int minutes = 0;
+            bool HoursCheck = false;
+            bool MinutesCheck = false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Please input the hours of the analogue clock! (1-12): ");
+                hours = int.Parse(Console.ReadLine());
+                if (hours <= 12 && hours >= 1)
+                {
+                    HoursCheck = true;
+                    break;
+                }
+                else{
+                    Console.WriteLine("Invalid Input type you have only " + (5 - (i+1)) + " chances");
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter minutes (0-59): ");
+                minutes = int.Parse(Console.ReadLine());
+                if (minutes <= 59 && minutes >= 0)
+                {
+                    MinutesCheck = true;
+                    break;
+                }
+                else{
+                    Console.WriteLine("Invalid Input type you have only " + (5 - (i+1)) + " chances");
+                }
+            }
+
+            if (HoursCheck && MinutesCheck)
+            {
+                
+                int hourHandAngle = (hours % 12) * 30 + (minutes / 2);
+                int minuteHandAngle = minutes * 6;
+                int angle = Math.Abs(hourHandAngle - minuteHandAngle);
+                if (angle > 180)
+                {
+                    angle = 360 - angle;
+                }
+
+                Console.WriteLine("The lesser angle between the hour hand and minute hand is {angle} degrees.");
+            }
+            else
+            {
+                Console.WriteLine("You have inputted wrong. Come back and try again ;)");
+            }
+        }
+    }
+}
---
+++ Question2/2ndQuestion.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace BranchStructure
+{
+    class Branch
+    {
+        public List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+    }
+
+    class Program
+    {
+        static int CalculateDepth(Branch branch)
+        {
+            int maxDepth = 0;
+            foreach (Branch b in branch.branches)
+            {
+                int depth = CalculateDepth(b);
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
+                }
+            }
+            return maxDepth + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            // Create the hierarchical structure
+            Branch root = new Branch();
+            for (int i = 0; i < 2; i++)
+            {
+                Branch branch1 = new Branch();
+                for (int j = 0; j < 2; j++)
+                {
+                    Branch branch2 = new Branch();
+                    for (int k = 0; k < 2; k++)
+                    {
+                        Branch branch3 = new Branch();
+                        branch2.branches.Add(branch3);
+                    }
+                    branch1.branches.Add(branch2);
+                }
+                root.branches.Add(branch1);
+            }
+
+            int depth = CalculateDepth(root);
+            Console.WriteLine($"The depth of the structure is {depth}");
+        }
+    }
+}
